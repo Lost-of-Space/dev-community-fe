8261dce453cb8c6e2fa3fe901754f30e9abac24d
Lost-of-Space <olexandchopey@gmail.com>
2025-06-15 17:55:35 +0300

    v0.5.9 - 404 on post page fix

--- src/pages/post.page.jsx
+++ src/pages/post.page.jsx
@@ -12,6 +12,7 @@ import { credentialHeaders } from '~/services/credentials'
 import { useTranslation } from "react-i18next";
 import { useLocalizedDateUtils } from "../common/date";
 import usePostViewTracker from "../common/post-view-tracker";
+import PageNotFound from "./404.page";
 
 
 export const postStructure = {
@@ -39,6 +40,8 @@ const PostPage = () => {
   const [commentsWrapper, setCommentsWrapper] = useState(false);
   const [totalParentCommentsLoaded, setTotalParentCommentsLoaded] = useState(0);
 
+  const [notFound, setNotFound] = useState(false);
+
   let { title, tags, content, banner, author: { personal_info: { fullname, username: author_username, profile_img } }, publishedAt } = post;
 
   const fetchPost = () => {
@@ -65,7 +68,7 @@ const PostPage = () => {
         setLoading(false);
       })
       .catch(err => {
-        console.log(err);
+        setNotFound(true);
         setLoading(false);
       })
   }
@@ -88,72 +91,72 @@ const PostPage = () => {
   return (
     <AnimationWrapper>
       {
-        loading ? <Loader />
-          :
-          <PostContext.Provider value={{ post, setPost, isLikedByUser, setIsLikedByUser, commentsWrapper, setCommentsWrapper, totalParentCommentsLoaded, setTotalParentCommentsLoaded }}>
-            <CommentsContainer />
+        loading ? <Loader /> :
+          notFound ? <PageNotFound /> :
+            (<PostContext.Provider value={{ post, setPost, isLikedByUser, setIsLikedByUser, commentsWrapper, setCommentsWrapper, totalParentCommentsLoaded, setTotalParentCommentsLoaded }}>
+              <CommentsContainer />
 
-            <div className="max-w-[900px] center py-10 max-lg:px-[5vw]">
-              <img src={banner} className="aspect-video" alt="post banner" />
-              <div className="flex flex-wrap mt-5 -mb-6 gap-3">
-                {
-                  tags.map((tag, i) => (
-                    <NavLink to={`/search/tag=${tag}`} key={i} className="tag text-black py-1 ml-2 px-4 rounded-full text-xs hover:bg-black hover:text-white">{tag}</NavLink>
-                  ))
-                }
-              </div>
-
-              <div className="mt-12">
-                <h2>{title}</h2>
-
-                <div className="flex max-sm:flex-col justify-between my-8">
-                  <div className="flex gap-5 items-start">
-                    <img src={profile_img} className="w-12 h-12 rounded-full" alt="author avatar" />
-                    <p className="capitalize text-xl">
-                      {fullname}
-                      <br />
-                      <span className="text-dark-grey">
-                        @
-                        <Link className="text-base text-dark-grey hover:underline" to={`/user/${author_username}`}>{author_username}</Link>
-                      </span>
-                    </p>
-                  </div>
-                  <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">{t("Published on")} {getDay(publishedAt)}</p>
+              <div className="max-w-[900px] center py-10 max-lg:px-[5vw]">
+                <img src={banner} className="aspect-video" alt="post banner" />
+                <div className="flex flex-wrap mt-5 -mb-6 gap-3">
+                  {
+                    tags.map((tag, i) => (
+                      <NavLink to={`/search/tag=${tag}`} key={i} className="tag text-black py-1 ml-2 px-4 rounded-full text-xs hover:bg-black hover:text-white">{tag}</NavLink>
+                    ))
+                  }
                 </div>
-              </div>
-              <PostInteraction />
 
-              <div className="my-12 ">
-                {
-                  content[0].blocks.map((block, i) => {
-                    return <div>
-                      <PostContent block={block} />
+                <div className="mt-12">
+                  <h2>{title}</h2>
+
+                  <div className="flex max-sm:flex-col justify-between my-8">
+                    <div className="flex gap-5 items-start">
+                      <img src={profile_img} className="w-12 h-12 rounded-full" alt="author avatar" />
+                      <p className="capitalize text-xl">
+                        {fullname}
+                        <br />
+                        <span className="text-dark-grey">
+                          @
+                          <Link className="text-base text-dark-grey hover:underline" to={`/user/${author_username}`}>{author_username}</Link>
+                        </span>
+                      </p>
                     </div>
-                  })
+                    <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">{t("Published on")} {getDay(publishedAt)}</p>
+                  </div>
+                </div>
+                <PostInteraction />
+
+                <div className="my-12 ">
+                  {
+                    content[0].blocks.map((block, i) => {
+                      return <div>
+                        <PostContent block={block} />
+                      </div>
+                    })
+                  }
+                </div>
+
+                <hr className="text-dark-grey/20" />
+
+                {
+                  similarPosts != null && similarPosts.length ?
+                    <>
+                      <h1 className="text-2xl mt-14 mb-10 font-medium">{t("Check similar posts")}:</h1>
+                      {
+                        similarPosts.map((post, i) => {
+                          let { author: { personal_info } } = post;
+
+                          return <AnimationWrapper key={i} transition={{ duration: 1, delay: i * 0.08 }} className="isolate">
+                            <PostCard content={post} author={personal_info} />
+                          </AnimationWrapper>
+                        })
+                      }
+                    </>
+                    :
+                    ""
                 }
               </div>
-
-              <hr className="text-dark-grey/20" />
-
-              {
-                similarPosts != null && similarPosts.length ?
-                  <>
-                    <h1 className="text-2xl mt-14 mb-10 font-medium">{t("Check similar posts")}:</h1>
-                    {
-                      similarPosts.map((post, i) => {
-                        let { author: { personal_info } } = post;
-
-                        return <AnimationWrapper key={i} transition={{ duration: 1, delay: i * 0.08 }} className="isolate">
-                          <PostCard content={post} author={personal_info} />
-                        </AnimationWrapper>
-                      })
-                    }
-                  </>
-                  :
-                  ""
-              }
-            </div>
-          </PostContext.Provider>
+            </PostContext.Provider>)
       }
     </AnimationWrapper>
   )
